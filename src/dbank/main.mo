dbaec521dae2b9751e450140ecb01b25f005f0db
ghihm0830 <146742222+ghihm0830@users.noreply.github.com>
2024-02-24 13:41:31 -0500

    first commit

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,57 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+import Int "mo:base/Int";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  stable var currentValue: Float = 300; //by default numbers are Nat //stable to make value persisted(flexible)-> not gonna be reset whenever being deployed (restore previous state)
+  currentValue := 300; //change value (var not let) // := regardless what previous value was, it will go back to 100 when ebing deployed/turn it off
+  Debug.print(debug_show (currentValue));
+
+  let id = 123412555664; //same as const value never changed
+  // Debug.print(debug_show (id));
+
+  stable var startTime = Time.now(); //current time in nanosecond since 1971/1/1
+  startTime := Time.now();
+  Debug.print(debug_show (startTime));
+
+
+  public func topUp(amount: Float) {
+    currentValue += amount;
+    Debug.print(debug_show (currentValue)); //print changed value in the console
   };
-};
+  // topUp(); //call the function internally //to call the function externally must use public
+  // must give data type (Nat) if the function has return value
+
+  public func withdraw(amount: Float) {
+    let tempValue: Float = currentValue - amount; //Int can be any whole number in possitive and negative //to give same type to the subtracted amount
+    //prevent violation of type error, use conditionals
+    if (tempValue >= 0) {
+      currentValue -= amount;
+      Debug.print(debug_show (currentValue));
+    } else {
+      Debug.print(debug_show ("Amount is too large. Current value is less than zero."));
+    }
+  };
+
+  public query func checkBalance(): async Float {
+    return currentValue; //query: read only cannot change anything
+  }; //output (return value) mnust come out asyncronously
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000; //nanosecond in second
+
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS)); 
+    //type of numbers in the equation (timeElapedS) must match to be calculated 1.01(float) //money should be in float not whole number(nat)
+
+    startTime := currentTime;
+    //Reset startTime(beginning of program running) and change to currentTime to compund(add) money on the money compunded in the previous time
+
+  }
+
+
+
+}
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,55 @@
-import { dbank } from "../../declarations/dbank";
+//bridge between index.js and main.mo
+import {dbank} from "../../declarations/dbank"; //access to the functions I made in main.mo and call them here
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+//update current value whenever page is loaded by updating span (index.html)
+window.addEventListener("load", async function() {
+  // console.log("Finished loading")
+  update();
+}); //used window to get current balance whenever page is loaded
 
-  const name = document.getElementById("name").value.toString();
+//listen from the form
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault(); //prevent page from being reloaded when button clicked
 
-  button.setAttribute("disabled", true);
+  // console.log("submitted")
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
+  //Disable button while change is coming up in UI
+  const button = event.target.querySelector("#submit-btn");
 
+
+  //what users typed in the forms
+  const inputAmount = parseFloat(document.getElementById("input-amount").value); //by default value comes out as integer but should change to float as I set up in main.mo (use parseFloat)
+  const withdrawAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  //Disable button
+  button.setAttribute("disabled", true); //create class in index.html
+
+  //trigger functions topUp and widthdraw in main.mo
+  if (document.getElementById("input-amount").value.length != 0) {
+    await dbank.topUp(inputAmount); 
+  };//!= 0 means user typed something
+
+  if (document.getElementById("withdrawal-amount").value.length != 0) {
+    await dbank.withdraw(withdrawAmount);
+  }; //can use else
+  
+  //compund balance by interest after user add or withdraw money
+  await dbank.compound();
+
+  //to see new balance in UI check balance and update
+  update();
+
+  //remove input value is removed once it has been uploaded
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+
+  //as soon as the value is updated, the button is back to active
   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+}); //triggered when submit button clicked
 
-  return false;
-});
+//remove repetitive function above
+async function update() {
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100; //devided by 100 to get two decimal places
+};
